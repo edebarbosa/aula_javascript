1065f3304217cd55558ad245b09b5a4848af4fe6
Edenilson <edebarbosa@gmail.com>
2021-01-06 00:59:38 -0300

    Aula JS

---
+++ js/main.js
@@ -0,0 +1,53 @@
+//function soma(n1, n2){
+//    return n1 + n2;
+//}
+//alert(soma(5, 20));
+
+//var d = new Date;{
+//    alert(d.getMinutes());
+//}
+
+//var count;
+//for(count=0; count <=10; count++){
+//    alert(count);
+//};
+
+//var count = 0;
+//while(count <= 5){
+//    console.log(count);
+//    alert(count);
+//    count++;
+//};
+
+
+
+
+//var idade = prompt("Qual a sua idade");
+//var idade = 18;
+//if(idade >=18){
+//    alert("Maior de idade");
+//}else{
+//    alert("Menor de idade");
+//}
+
+//var frutas = [{nome:"maçã" , cor:"vermelha"}, {nome:"uva" , cor:"roxa"}]
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+/*
+var fruta = {nome:"maçã" , cor:"vermelha"}
+console.console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var nome = "Edenilson Rocha"; 
+//var idade = 44;
+//var idade2 =10;
+//alert(nome + " tem " + idade + " Anos");
+//alert(idade + idade2 )
+//var frase = "Japão é o melhor time do mundo";
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("Japào", "Brasil"));
+//alert(frase.replace("melhor", "pior"));
+
